d6b2a7f654e89b82b5fb1f92d9394a7b568046c7
greatapp <groomrent@gmail.com>
2026-03-22 17:05:18 +0530

    Login Screen design

---
+++ app/src/main/java/com/learning/exp/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.learning.exp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.learning.exp.databinding.LoginActivityBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var mBinding: LoginActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = LoginActivityBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+
+    }
+
+}
